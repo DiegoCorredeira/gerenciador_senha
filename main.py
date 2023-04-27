cf077f1eeb229aa3d4e7bb6053d49b735cfb9110
Diego <diegosccpinto@gmail.com>
2023-04-26 21:52:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -37,45 +37,63 @@ def remove_password(site):
     conn.commit()
 
 
-flag = ''
-while flag != 'sair':
+def menu():
     print("1 - Adicionar nova senha")
     print("2 - Buscar senha")
     print("3 - Atualizar senha")
     print("4 - Remover senha")
     print("5 - Encerrar programa")
+
+
+def add():
+    site = input('Informe o nome do site: ')
+    username = input('Informe o nome de usuário: ')
+    password = input('Informe a senha: ')
+    add_passwords(site, username, password)
+    print('Senha cadastrada com sucesso!')
+
+
+def search():
+    site = input('Informe o nome do site que deseja buscar a senha: ')
+    result = search_passwords(site)
+    if result:
+        print(result['Username'])
+        print(result['password'])
+    else:
+        print('Senha não encontrada')
+
+
+def update():
+    site = input('Informe o site que deseja atualizar a senha: ')
+    username = input('Informe o nome de usuário: ')
+    password = input('Informe a nova senha:')
+    update_password(site, username, password)
+
+
+def remove():
+    site = input('Informe o site que deseja remover:')
+    remove_password(site)
+    print('Senha removida com sucesso!')
+
+options = {
+    1: add,
+    2: search,
+    3: update,
+    4: remove
+}
+
+while True:
+    menu()
     choice = input('Informe a opção desejada: ')
     try:
         choice = int(choice)
     except:
         print('Opção precisa ser um número inteiro.')
 
-    if choice == 1:
-        site = input('Informe o nome do site: ')
-        username = input('Informe o nome de usuário: ')
-        password = input('Informe a senha: ')
-
-        add_passwords(site, username, password)
-        print('Senha cadastrada com sucesso!')
-    elif choice == 2:
-        site = input('Informe o nome do site que deseja buscar a senha: ')
-        result = search_passwords(site)
-        if result:
-            print(result['Username'])
-            print(result['password'])
-        else:
-            print('Senha não encontrada')
-    elif choice == 3: 
-        site = input('Informe o site que deseja atualizar a senha: ')
-        username = input('Informe o nome de usuário: ')
-        password = input('Informe a nova senha:') 
-        update_password(site, username, password)
-    elif choice == 4: 
-        site = input('Informe o site que deseja remover:')
-        remove_password(site)
-        print('Senha removida com sucesso!')
-    elif choice == 5:
-        flag = 'sair'
+    if choice == 5:
         print('Encerrando programa.')
-    else: 
+        break
+    elif choice in options:
+        options[choice]()
+    else:
         print('Escolha uma opção válida.')
